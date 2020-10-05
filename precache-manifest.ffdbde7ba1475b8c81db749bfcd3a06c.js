1a798dc4f5be37335a9b2ce9b6e645795c6d1865
taze-fullstack <taze@fullstack.ph>
2020-10-05 21:56:57 +0800

    Updates

---
+++ precache-manifest.ffdbde7ba1475b8c81db749bfcd3a06c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a10a8a5dd6a7195e018f6d5f663fbd3",
+    "url": "/service-worker-test/index.html"
+  },
+  {
+    "revision": "eff1c15fbd18ef0a88e6",
+    "url": "/service-worker-test/static/css/main.9eaa902e.chunk.css"
+  },
+  {
+    "revision": "85d2461bf3fd269b6e5d",
+    "url": "/service-worker-test/static/js/2.57276352.chunk.js"
+  },
+  {
+    "revision": "60037e8887f425d039bd63775efa9319",
+    "url": "/service-worker-test/static/js/2.57276352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eff1c15fbd18ef0a88e6",
+    "url": "/service-worker-test/static/js/main.56391010.chunk.js"
+  },
+  {
+    "revision": "d4999ca05ac10dda5d21",
+    "url": "/service-worker-test/static/js/runtime-main.6976ba9d.js"
+  }
+]);
